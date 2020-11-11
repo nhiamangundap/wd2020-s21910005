bbcad5ee27513afe5944a4273ec7b683b7ce279d
nhiamangundap <69839791+nhiamangundap@users.noreply.github.com>
2020-11-11 13:46:07 +0800

    variabel dan operator js

---
+++ pertemuan23/index.js
@@ -0,0 +1,6 @@
+let umur = 18;
+let namaDepan = "Nathania";
+let namaBelakang = "Mangundap";
+console.log(
+    "Nama Saya" + namaDepan + " " + namaBelakang + ". umur saya adalah " + umur + "tahun"
+); 
